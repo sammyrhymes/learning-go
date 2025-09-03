41d95e9127571ce5b67c046078ef5acbfde653d0
sammyrhymes <samuelmwendwa5996@gmail.com>
2025-09-03 12:53:48 +0300

    Add a program to check if there are duplicates in the user's input

---
+++ GO-001-Tutorial/1.3 Dup1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	count := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		count[input.Text()]++
+	}
+	for line, n := range count {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
